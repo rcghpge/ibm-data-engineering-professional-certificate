73a88b4fff8e6a51ba5674737569370c91fed45c
Robert <121340777+rcghpge@users.noreply.github.com>
2024-05-28 23:56:39 -0500

    Add files via upload

---
+++ python-project-for-de/labs/sample2.py
@@ -0,0 +1,10 @@
+# install pylint library and run script in terminal
+
+def add(number1, number2):
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
